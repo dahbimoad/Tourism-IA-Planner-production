c3976c7657c881c4fc4ec8ce2c8f336ebd81532f
OuaailElaouad <2003waelelaouad@gmail.com>
2024-12-19 02:39:53 +0100

    Adding the login and signup pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
 
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Router>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+      {/* Ajoutez d'autres routes ici */}
+    </Routes>
+  </Router>
   );
 }
 
