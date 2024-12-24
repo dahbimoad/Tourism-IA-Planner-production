7991e8c0ddc7f14c370b3a4896f2f7272b7ffa9d
OUMIF <eladnanimehdi03@gmail.com>
2024-12-24 22:50:33 +0100

    j ai ajouter un log dans main.py afin de suivre l'etat de la migration

--- Server/main.py
+++ Server/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from app.controllers.preferencesController import router as preferences_router
 from app.controllers.VilleController import router as villes_router
+from app.db.database import Base, engine
+import logging
+logging.basicConfig()
+logging.getLogger('sqlalchemy').setLevel(logging.INFO)
+
 
 app = FastAPI()
+
+
+
+@app.on_event("startup")
+def startup_event():
+    Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
