5520937a8145a746061d06e35b20cf3338366b35
imadechchriki <imad.echchriki@gmail.com>
2024-12-24 12:26:54 +0100

    linked api login et signup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,20 +1,32 @@
-import { useState } from 'react';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./components/LoginPage";
-import SignupPage from "./components/SignupPage";
+import PrivateRoute from "./components/PrivateRoute";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { AuthProvider } from './contexts/AuthContext';
+import Dashboard from "./pages/Dashboard";
 
 
 function App() {
   return (
-    <Router>
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/dashboard" element={<h1>Dashboard</h1>} />
-      {/* Ajoutez d'autres routes ici */}
-    </Routes>
-  </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          {/* Utilisation de PrivateRoute pour protéger /dashboard */}
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                  <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          {/* Ajoutez d'autres routes ici */}
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
