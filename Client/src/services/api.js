eecaacfe526cd0e40f8ec8c4bbc4d9e612bc43ee
dahbimoad <dahbimoad1@gmail.com>
2024-12-26 20:40:27 +0100

    modifying cors and api link

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/';
+const API_URL = 'https://tourism-ia-planner-production.onrender.com/';
 
 
 export const getMessageFromAPI = async () => {
