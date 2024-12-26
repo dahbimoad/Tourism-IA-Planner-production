fba0621f16d56acd60ae23f9aa1df714b40c8217
dahbimoad <dahbimoad1@gmail.com>
2024-12-26 19:20:15 +0100

    commenting path filter in pipeline

--- Server/main.py
+++ Server/main.py
@@ -41,7 +41,7 @@ def startup_event():
 
 @app.get("/")
 def root_endpoint():
-    return {"message": "helloooo"}
+    return {"message": "hellooooaaaaaaaaaaaaaaaa"}
 
 app.include_router(preferences_router)
 app.include_router(villes_router)
