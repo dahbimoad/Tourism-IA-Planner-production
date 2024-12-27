5904a3e69cd087f0e3f762bbe73ed703ae956edd
dahbimoad <dahbimoad1@gmail.com>
2024-12-27 01:40:43 +0100

    adding env for client

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://tourism-ia-planner-production.onrender.com/';
+const API_URL = import.meta.env.VITE_API_URL;
 
 
 export const getMessageFromAPI = async () => {
