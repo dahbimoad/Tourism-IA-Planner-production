2574b2fafef845a727e6272f4087d4c0260f43bb
muaddd <114266524+dahbimoad@users.noreply.github.com>
2025-01-04 19:45:46 +0100

    Update tailwind.config.js

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,10 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+import forms from '@tailwindcss/forms'
+import daisyui from 'daisyui'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    forms,
+    daisyui
   ],
-};
+}
